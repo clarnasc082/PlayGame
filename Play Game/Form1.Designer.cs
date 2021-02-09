a6956599200a19e9513a3dead54608ec073c58ca
Clara Nascu <clarnasc082@ed.amdsb.ca>
2021-02-09 10:12:16 -0500

    Add project files.

---
+++ Play Game/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Play_Game
+{
+    partial class goLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.gameStartLabel = new System.Windows.Forms.Label();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.MenuText;
+            this.startButton.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(365, 117);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(164, 63);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Play Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // gameStartLabel
+            // 
+            this.gameStartLabel.AutoSize = true;
+            this.gameStartLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameStartLabel.Location = new System.Drawing.Point(352, 207);
+            this.gameStartLabel.Name = "gameStartLabel";
+            this.gameStartLabel.Size = new System.Drawing.Size(188, 24);
+            this.gameStartLabel.TabIndex = 1;
+            this.gameStartLabel.Text = "Game will start in:";
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLabel.Location = new System.Drawing.Point(546, 207);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(22, 24);
+            this.numberLabel.TabIndex = 2;
+            this.numberLabel.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(413, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GO!";
+            this.label1.Visible = false;
+            // 
+            // goLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(922, 488);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.gameStartLabel);
+            this.Controls.Add(this.startButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "goLabel";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label gameStartLabel;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Play Game/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Play_Game
+{
+    public partial class goLabel : Form
+    {
+        public goLabel()
+        {
+            InitializeComponent();//
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+            SoundPlayer doorSound = new SoundPlayer(Properties.Resources.Store_Door_Chime_Mike_Koenig_570742973);
+            doorSound.Play();
+
+            startButton.Visible = false;
+           
+            Refresh();
+            Thread.Sleep(1000);
+
+            numberLabel.Text = "2";
+            Refresh();
+            Thread.Sleep(1000);
+
+            numberLabel.Text = "1";
+            Refresh();
+            Thread.Sleep(1000);
+
+            numberLabel.Visible = false; 
+            
+            gameStartLabel.Visible = false;
+            this.BackColor = Color.Green;
+            Refresh();
+            Thread.Sleep(1000);
+
+            label1.Visible = true;
+
+        }
+    }
+}
